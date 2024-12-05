df71541dbd5e1a8fcd13097ee77930e6e3a60bcc
Henry <a54321580@yahoo.com.tw>
2024-12-05 15:03:23 +0800

    改寫更新productimages

--- app/Http/Controllers/Back/ProductOptionController.php
+++ app/Http/Controllers/Back/ProductOptionController.php
@@ -163,92 +163,166 @@ class ProductOptionController extends Controller
     }
 
     //增刪改產品選項對應的圖片
+    // public function updateProductImages(Request $request)
+    // {
+
+    //     $validated = $request->validate([
+    //         'po_id' => 'required|integer|exists:product_options,id',
+    //         'product_images' => 'required|array',
+    //         'product_images.*.alt_text' => 'required|string|max:255',
+    //         'product_images.*.is_combination' => 'required|boolean',
+    //         'product_images.*.image' => 'nullable|image',
+    //     ]);
+
+    //     $productOptionId = $request->input('po_id');
+    //     $productOption = ProductOption::find($productOptionId);
+    //     $product_images = $productOption->productImages()->get();
+
+    //     // 獲取當前 product_id 下的最大 order 值
+    //     $currentMaxOrder = ProductImage::where('product_id', $productOption->product_id)
+    //         ->max('order') ?? 0;
+
+
+    //     //拿到product_options對應的images
+    //     $productImagesIdsShouldBeRemove = $productOption->productImages->map(function ($productImage) {
+    //         return $productImage->id;
+    //     })->toArray();
+
+
+
+    //     $product_images = $validated['product_images'];
+    //     $new_product_images = []; //給新的dom用的
+
+    //     foreach ($product_images as $id => $product_image) {
+    //         //處理有按上傳的圖片
+    //         if (isset($product_image['image'])) {
+    //             $name = mt_rand() . '_' . $product_image['image']->getClientOriginalName();
+    //             $path = '/storage/' . $product_image['image']->storeAs(
+    //                 'product_options',
+    //                 $name,
+    //                 'public',
+    //             );
+    //             $product_image['image'] = $path;
+    //         }
+
+    //         //新的dom走if，原本存在的走else
+    //         //product_options[new_1][name]
+    //         if (strpos($id, 'new_') !== false) {
+    //             // Log::info($product_image);
+    //             $product_image['product_id'] = $productOption->product_id;
+    //             // 設置 order 為當前最大值加 1
+    //             $currentMaxOrder++;
+    //             $product_image['order'] = $currentMaxOrder;
+    //             $product_image['is_combination'] = 0;
+    //             // Log::info($currentMaxOrder);
+    //             array_push($new_product_images, new ProductImage($product_image));
+    //         } else {
+    //             $currentProductImage = ProductImage::where('id', $id)->first();
+    //             if ($currentProductImage) {
+    //                 if (isset($product_image['image'])) {
+    //                     //更新圖片
+    //                     $path = str_replace('/storage/', '', $currentProductImage->image);
+    //                     if (Storage::disk('public')->exists($path)) {
+    //                         Storage::disk('public')->delete($path);
+    //                     } else {
+    //                         Log::info("Image does not exist.");
+    //                     }
+    //                 }
+    //                 //更新現有的資料
+    //                 $currentProductImage->update($product_image);
+    //                 //拿到資料庫但沒有被更新的id = 在畫面上被X的id
+    //                 $productImagesIdsShouldBeRemove = array_diff(
+    //                     $productImagesIdsShouldBeRemove, //原本資料庫中的options id
+    //                     [$currentProductImage->id]  //dom節點拿到的 id (被更新的)
+    //                 );
+    //             }
+    //         }
+    //     }
+
+    //     //新增新的product_options
+    //     // Log::info('123',$product->product_options()->saveMany($new_product_options));
+    //     Log::info($new_product_images);
+    //     $productOption->productImages()->saveMany($new_product_images);
+
+    //     // Log::info($productImagesIdsShouldBeRemove);
+    //     DB::table('product_images')->whereIn('id', $productImagesIdsShouldBeRemove)->delete();
+    // }
+
     public function updateProductImages(Request $request)
     {
-
         $validated = $request->validate([
             'po_id' => 'required|integer|exists:product_options,id',
             'product_images' => 'required|array',
+            'product_images.*.id' => 'nullable|integer|exists:product_images,id',
             'product_images.*.alt_text' => 'required|string|max:255',
             'product_images.*.is_combination' => 'required|boolean',
             'product_images.*.image' => 'nullable|image',
+            'product_images.*._delete' => 'nullable|boolean',
         ]);
 
-        $productOptionId = $request->input('po_id');
-        $productOption = ProductOption::find($productOptionId);
-        $product_images = $productOption->productImages()->get();
+        // 找到對應的 Product Option
+        $productOption = ProductOption::findOrFail($validated['po_id']);
 
-        // Log::info($productOption->productImages->id);
+        // 拿到所有的 product_images 並刪除未被更新的項目
+        $currentMaxOrder = ProductImage::where('product_id', $productOption->product_id)->max('order') ?? 0;
 
-        // 獲取當前 product_id 下的最大 order 值
-        $currentMaxOrder = ProductImage::where('product_id', $productOption->product_id)
-            ->max('order') ?? 0;
+        // product_images 進行新增/更新/刪除
+        foreach ($validated['product_images'] as $productImage) {
+            if (!empty($productImage['_delete']) && isset($productImage['id'])) {
+                // 刪除圖片資料
+                $existingImage = ProductImage::findOrFail($productImage['id']);
+                $path = str_replace('/storage/', '', $existingImage->image);
+                if (Storage::disk('public')->exists($path)) {
+                    Storage::disk('public')->delete($path);
+                }
+                $existingImage->delete();
+            } elseif (isset($productImage['id'])) {
+                // 更新圖片資料
+                $existingImage = ProductImage::findOrFail($productImage['id']);
+                $existingImage->update([
+                    'alt_text' => $productImage['alt_text'],
+                    'is_combination' => $productImage['is_combination'],
+                ]);
 
+                if (isset($productImage['image'])) {
+                    // 刪除舊圖片並更新新圖片
+                    $path = str_replace('/storage/', '', $existingImage->image);
+                    if (Storage::disk('public')->exists($path)) {
+                        Storage::disk('public')->delete($path);
+                    }
 
-        //拿到product_options對應的images
-        $productImagesIdsShouldBeRemove = $productOption->productImages->map(function ($productImage) {
-            return $productImage->id;
-        })->toArray();
-
-        // return response()->json($productImagesIdsShouldBeRemove);
-        // return response()->json($validated['product_images']);
-
-        if (isset($validated)) {
-            $product_images = $validated['product_images'];
-            $new_product_images = []; //給新的dom用的
-
-            foreach ($product_images as $id => $product_image) {
-                //處理有按上傳的圖片
-                if (isset($product_image['image'])) {
-                    $name = mt_rand() . '_' . $product_image['image']->getClientOriginalName();
-                    $path = '/storage/' . $product_image['image']->storeAs(
+                    $name = mt_rand() . '_' . $productImage['image']->getClientOriginalName();
+                    $path = '/storage/' . $productImage['image']->storeAs(
                         'product_options',
                         $name,
-                        'public',
+                        'public'
                     );
-                    $product_image['image'] = $path;
+                    $existingImage->update(['image' => $path]);
+                }
+            } else {
+                // 新增圖片
+                $path = null;
+                if (isset($productImage['image'])) {
+                    $name = mt_rand() . '_' . $productImage['image']->getClientOriginalName();
+                    $path = '/storage/' . $productImage['image']->storeAs(
+                        'product_options',
+                        $name,
+                        'public'
+                    );
                 }
 
-                //新的dom走if，原本存在的走else
-                //product_options[new_1][name]
-                if (strpos($id, 'new_') !== false) {
-                    // Log::info($product_image);
-                    $product_image['product_id'] = $productOption->product_id;
-                    // 設置 order 為當前最大值加 1
-                    $currentMaxOrder++;
-                    $product_image['order'] = $currentMaxOrder;
-                    $product_image['is_combination'] = 0;
-                    // Log::info($currentMaxOrder);
-                    array_push($new_product_images, new ProductImage($product_image));
-                } else {
-                    $currentProductImage = ProductImage::where('id', $id)->first();
-                    if ($currentProductImage) {
-                        if (isset($product_image['image'])) {
-                            //更新圖片
-                            $path = str_replace('/storage/', '', $currentProductImage->image);
-                            if (Storage::disk('public')->exists($path)) {
-                                Storage::disk('public')->delete($path);
-                            } else {
-                                Log::info("Image does not exist.");
-                            }
-                        }
-                        //更新現有的資料
-                        $currentProductImage->update($product_image);
-                        //拿到資料庫但沒有被更新的id = 在畫面上被X的id
-                        $productImagesIdsShouldBeRemove = array_diff(
-                            $productImagesIdsShouldBeRemove, //原本資料庫中的options id
-                            [$currentProductImage->id]  //dom節點拿到的 id (被更新的)
-                        );
-                    }
-                }
+                $currentMaxOrder++;
+                $productOption->productImages()->create([
+                    'product_option_id' => $productOption->id,
+                    'product_id' => $productOption->product_id,
+                    'alt_text' => $productImage['alt_text'],
+                    'is_combination' => $productImage['is_combination'],
+                    'image' => $path,
+                    'order' => $currentMaxOrder,
+                ]);
             }
-
-            //新增新的product_options
-            // Log::info('123',$product->product_options()->saveMany($new_product_options));
-            Log::info($new_product_images);
-            $productOption->productImages()->saveMany($new_product_images);
         }
-        // Log::info($productImagesIdsShouldBeRemove);
-        DB::table('product_images')->whereIn('id', $productImagesIdsShouldBeRemove)->delete();
+
+        return response()->json(['message' => 'Product images updated successfully.']);
     }
 }
